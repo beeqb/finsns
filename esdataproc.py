23a28b2587c018f49f52d78c25ab327ffbeae0f1
Yue Wu <wuyue681@gmail.com>
2017-08-18 11:08:20 +0800

    FIX format. Finish get_content.

--- esdataproc.py
+++ esdataproc.py
@@ -5,14 +5,17 @@ N_FILES = 3
 PREFIX = 't'
 FIELDNAMES = ["id","crawler_time","is_retweet","user_id","nick_name","tou_xiang","user_type","weibo_id","weibo_content","zhuan","ping","zan","url","device","locate","time","pic_content"]
 
+
 def readfile(filename):
     with open(filename, 'r') as f:
         return json.load(f)
+
 
 def initcsv(csvfile):
     writer = csv.DictWriter(csvfile, fieldnames=FIELDNAMES)
     writer.writeheader()
     return writer
+
 
 def writecsv(writer, data):
     for i in range(len(data)):
--- news_sina_crawler.py
+++ news_sina_crawler.py
@@ -10,8 +10,10 @@ import lxml.html
 URL = 'http://search.sina.com.cn/?c=news&q=%s&range=all&time=custom&stime=2013-12-01&etime=2017-08-17&num=20&col=1_7&page=%d'
 STOCKS_FILE = 'shangzheng180.csv'
 
+
 def get_stocks(f):
-    csv_data = csv.reader(f, encoding='gbk')
+#    csv_data = csv.reader(f, encoding='gbk')
+    csv_data = csv.reader(f, encoding='utf-8')
     stocks = []
     names = []
     for row in csv_data:
@@ -19,13 +21,19 @@ def get_stocks(f):
         names.append(row[1])
     return stocks, names
 
+
 def crawler(keyword, pg):
-    url = URL % (urllib2.quote(keyword.encode('gbk')), pg)
+#    url = URL % (urllib2.quote(keyword.encode('gbk')), pg)
+    url = URL % (urllib2.quote(keyword.decode('utf-8').encode('gbk')), pg)
     page = urllib2.urlopen(url).read()
     return page
 
+
 def process_page(page, pg):
-    content = lxml.html.fromstring(page).getroot()
+#    content = lxml.html.fromstring(page).getroot()
+    with open('test.html', 'w') as f:
+        f.write(page)
+    content = lxml.html.fromstring(page)
     if pg == 1:
         t = content.cssselect('div#result div.l_v2')[0].text_content()
         total_num = int(''.join([s for s in t if s.isdigit()]))
@@ -45,19 +53,34 @@ def process_page(page, pg):
         times.append(meta[2])
         url = title_link.get('href')
         contents.append(get_content(url))
+    if pg == 1:
+        return total_num, titles, dates, times, sources, contents
+    else:
+        return titles, dates, times, sources, contents
+
 
 def get_content(url):
     page = urllib2.urlopen(url).read()
     dom_page = lxml.html.fromstring(page).getroot()
     content = dom_page.cssselect('div#artibody')
     if not content:
-        return ''
+        return url
+    article = []
+    for p in content.cssselect('p'):
+        article.append(p.text_content())
+    return article
+
 
 def main():
     with open(STOCKS_FILE, 'rb') as f:
         stocks, names = get_stocks(f)
     page = crawler(names[0], 1)
     total_num, titles, dates, times, sources, contents = process_page(page, 1)
+    print total_num
+    with open('%d' % stocks[0], 'wb') as f:
+        writer = csv.writer(f)
+        for title, date, time, source, content in zip(titles, dates, times, sources, contents):
+            writer.writerow(title, date, time, source, content)
     # for stock, name in zip(stocks, names):
     #     page = crawler(name, 1)
     #     with open('test10.html', 'w') as f:
