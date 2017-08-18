81225b205e738ebcf3b6f9ce390497ae33000017
Yue Wu <wuyue681@gmail.com>
2017-08-18 18:15:17 +0800

    finish news_sina_crawler.py

--- news_sina_crawler.py
+++ news_sina_crawler.py
@@ -5,15 +5,17 @@
 import urllib2
 import unicodecsv as csv
 import lxml.html
+from bs4 import BeautifulSoup
 
 
 URL = 'http://search.sina.com.cn/?c=news&q=%s&range=all&time=custom&stime=2013-12-01&etime=2017-08-17&num=20&col=1_7&page=%d'
 STOCKS_FILE = 'shangzheng180.csv'
+NUM_ITEMS = 20
 
 
 def get_stocks(f):
-#    csv_data = csv.reader(f, encoding='gbk')
-    csv_data = csv.reader(f, encoding='utf-8')
+    csv_data = csv.reader(f, encoding='gbk')
+#    csv_data = csv.reader(f, encoding='utf-8')
     stocks = []
     names = []
     for row in csv_data:
@@ -23,16 +25,13 @@ def get_stocks(f):
 
 
 def crawler(keyword, pg):
-#    url = URL % (urllib2.quote(keyword.encode('gbk')), pg)
-    url = URL % (urllib2.quote(keyword.decode('utf-8').encode('gbk')), pg)
+    url = URL % (urllib2.quote(keyword.encode('gbk')), pg)
+#    url = URL % (urllib2.quote(keyword.decode('utf-8').encode('gbk')), pg)
     page = urllib2.urlopen(url).read()
     return page
 
 
 def process_page(page, pg):
-#    content = lxml.html.fromstring(page).getroot()
-    with open('test.html', 'w') as f:
-        f.write(page)
     content = lxml.html.fromstring(page)
     if pg == 1:
         t = content.cssselect('div#result div.l_v2')[0].text_content()
@@ -46,11 +45,11 @@ def process_page(page, pg):
     for item in items:
         header = item.cssselect('h2')[0]
         title_link = header.cssselect('a')[0]
-        titles.append(title_link.text_content())
-        meta = header.cssselect('span')[0].text_content().split(' ')
-        sources.append(meta[0])
-        dates.append(meta[1])
-        times.append(meta[2])
+        titles.append(title_link.text_content().encode('utf-8'))
+        meta = header.cssselect('span.fgray_time')[0].text_content().split(' ')
+        sources.append(meta[0].encode('utf-8'))
+        dates.append(meta[1].encode('utf-8'))
+        times.append(meta[2].encode('utf-8'))
         url = title_link.get('href')
         contents.append(get_content(url))
     if pg == 1:
@@ -60,31 +59,40 @@ def process_page(page, pg):
 
 
 def get_content(url):
-    page = urllib2.urlopen(url).read()
-    dom_page = lxml.html.fromstring(page).getroot()
-    content = dom_page.cssselect('div#artibody')
+    try:
+        page = urllib2.urlopen(url).read()
+    except urllib2.URLError:
+        print url
+        return url
+    dom_page = BeautifulSoup(page, 'html.parser')
+    content = dom_page.select('div#artibody')
     if not content:
         return url
     article = []
-    for p in content.cssselect('p'):
-        article.append(p.text_content())
-    return article
+    for p in content[0].select('p'):
+        article.append(p.get_text())
+    return ''.join(article)
 
 
 def main():
     with open(STOCKS_FILE, 'rb') as f:
         stocks, names = get_stocks(f)
-    page = crawler(names[0], 1)
-    total_num, titles, dates, times, sources, contents = process_page(page, 1)
-    print total_num
-    with open('%d' % stocks[0], 'wb') as f:
-        writer = csv.writer(f)
-        for title, date, time, source, content in zip(titles, dates, times, sources, contents):
-            writer.writerow(title, date, time, source, content)
-    # for stock, name in zip(stocks, names):
-    #     page = crawler(name, 1)
-    #     with open('test10.html', 'w') as f:
-    #         f.write(page)
+
+    for stock, name in zip(stocks, names):
+        print 'Start getting %s:.....' % (stock.encode('gbk'))
+        with open('%s.csv' % stock.encode('gbk'), 'wb') as f:
+            writer = csv.writer(f)
+            page = crawler(name, 1)
+            total_num, titles, dates, times, sources, contents = process_page(page, 1)
+            print total_num
+            for title, date, time, source, content in zip(titles, dates, times, sources, contents):
+                writer.writerow([title, date, time, source, content])
+            if total_num > 20:
+                total_page = total_num / NUM_ITEMS + (total_num % NUM_ITEMS > 0)
+                for i in xrange(2, total_page+1):
+                    titles, dates, times, sources, contents = process_page(page, i)
+                    for title, date, time, source, content in zip(titles, dates, times, sources, contents):
+                        writer.writerow([title, date, time, source, content])
 
 
 if __name__ == '__main__':
