73beb29f98c16092be4fe2a1da1d9a57db21c824
Yue Wu <wuyue681@gmail.com>
2017-08-21 22:02:49 +0800

    Fix all bugs, new_sina_crawler finally works.

--- news_sina_crawler.py
+++ news_sina_crawler.py
@@ -17,17 +17,17 @@ NUM_ITEMS = 50
 
 def get_stocks(f):
     csv_data = csv.reader(f, encoding='gbk')
-#    csv_data = csv.reader(f, encoding='utf-8')
+    # csv_data = csv.reader(f, encoding='utf-8')
     stocks = []
     names = []
     for row in csv_data:
-        stocks.append(row[0])
-        names.append(row[1])
+        stocks.append(row[0].encode('utf-8'))
+        names.append(row[1].encode('utf-8'))
     return stocks, names
 
 
 def crawler(keyword, stime, etime, pg):
-    url = URL % (urllib2.quote(keyword), stime, etime, pg)
+    url = URL % (keyword, stime, etime, pg)
 #    url = URL % (urllib2.quote(keyword.decode('utf-8').encode('gbk')), pg)
     resp = urllib2.urlopen(url).read()
     return resp
@@ -37,6 +37,7 @@ def process_resp(resp, pg):
     content = (json.loads(resp))['result']
     if pg == 1:
         total_num = int(content['count'])
+        print content['q'], content['page'], total_num
     titles = []
     dates = []
     times = []
@@ -49,8 +50,6 @@ def process_resp(resp, pg):
         times.append(item['datetime'].split(' ')[1])
         url = item['url']
         contents.append(get_content(url))
-    if pg == 1:
-        print total_num
 
     return titles, dates, times, sources, contents
 
@@ -74,8 +73,12 @@ def get_content(url):
 def gen_time():
     years = [i for i in range(2013, 2018)]
     months = [i for i in range(1, 13)]
-    stime = ['%d-%d-1' % (i, j) for i, j in zip(years, months)]
-    etime = ['%d-%d-31' % (i, j) for i, j in zip(years, months)]
+    stime = []
+    etime = []
+    for y in years:
+        for m in months:
+            stime.append('%d-%d-1' % (y, m))
+            etime.append('%d-%d-31' % (y, m))
     return stime, etime
 
 
@@ -108,8 +111,8 @@ def main():
     stime, etime = gen_time()
 
     for stock, name in zip(stocks, names):
-        print 'Start getting %s:.....' % (stock.encode('gbk'))
-        with open('%s.csv' % stock.encode('gbk'), 'wb') as f:
+        print 'Start getting %s, %s:.....' % (stock, name)
+        with open('%s.csv' % stock, 'wb') as f:
             writer = csv.writer(f)
             start_crawler(name, stime, etime, writer)
 
