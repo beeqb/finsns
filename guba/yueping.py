af6dbe14929f595c9f498a9510982d18f4016e81
Yue Wu <wuyue681@gmail.com>
2018-01-28 16:31:13 +0800

    Add ids to result. Add header to output CSV. Handle all exceptions and print out msgs properly. Change the waiting time as 300 - duration.

--- guba/yueping.py
+++ guba/yueping.py
@@ -12,6 +12,8 @@ URL2 = 'http://guba.eastmoney.com/list,%s_2.html'
 TIEZI_URL = 'http://guba.eastmoney.com/'
 
 tiezi_list = {}
+tiezi_ids = {}
+
 
 def gcrawler(urls):
     rs = (grequests.get(u, timeout=10) for u in urls)
@@ -22,26 +24,31 @@ def get_codes(cf):
     return list(map(lambda x: x.rstrip(), cf.readlines()))
 
 
-def   init_tiezi_set(code):
-    urls = [URL1%(code), URL2%(code)]
+def init_tiezi_set(code):
+    urls = [URL1 % (code), URL2 % (code)]
     resps = gcrawler(urls)
     try:
         list_pages = list(map(lambda x: BeautifulSoup(x.text, 'html.parser'), resps))
     except:
         print(urls)
     tiezi_urls = extract_list_pages(list_pages)
+    for tu in tiezi_urls:
+        tiezi_ids[tu] = tu.split(',')[2].split('.')[0]
+
     return tiezi_urls
 
 
 def update_tiezi_set(code):
     gp_tiezi_set = tiezi_list[code]
-    urls = [URL1%(code)]
+    urls = [URL1 % (code)]
     resps = gcrawler(urls)
     try:
         list_pages = list(map(lambda x: BeautifulSoup(x.text, 'html.parser'), resps))
     except:
         print(urls)
     new_tiezi_urls = extract_list_pages(list_pages)
+    for tu in new_tiezi_urls:
+        tiezi_ids[tu] = tu.split(',')[2].split('.')[0]
     gp_tiezi_set.update(new_tiezi_urls)
     tiezi_list[code] = gp_tiezi_set
 
@@ -67,36 +74,41 @@ def extract_list_pages(list_pages):
 def get_yueping(urls):
     resps = gcrawler(urls)
     tiezis = []
-    for x in resps:
+    for idx, x in enumerate(resps):
         try:
             x.encoding = 'utf-8'
             tiezis.append(BeautifulSoup(x.text, 'html.parser'))
-        except:
+        except Exception as e:
+            print('Error: ' + urls[idx], 'URL error. ', e)
             continue
     yuedus = []
     pingluns = []
     titles = []
-    for t in tiezis:
+    t_ids = []
+    for u in urls:
+        t_ids.append(tiezi_ids[u])
+    for idx, t in enumerate(tiezis):
         zhuanping_doms = t.select('div#zwlist script')
         try:
             zhuanping_content = reduce(lambda x,y: x.text+y.text, zhuanping_doms)
-        except:
+        except Exception as e:
+            print('Error: ', urls[idx], 'None Zhuan Ping content. ', e)
             continue
         yuedu = re.search('var num=(.*?);.*', zhuanping_content).group(1)
-        pinglun_pattern =  re.compile('.*var pinglun_num=(.*?);.*', re.S)
-        pinglun= re.search(pinglun_pattern, zhuanping_content).group(1)
+        pinglun_pattern = re.compile('.*var pinglun_num=(.*?);.*', re.S)
+        pinglun = re.search(pinglun_pattern, zhuanping_content).group(1)
         title = t.select('div#zwconttbt')[0].text
         yuedus.append(yuedu)
         pingluns.append(pinglun)
         titles.append(title.strip())
 
-    return (titles, yuedus, pingluns)
+    return (t_ids, titles, yuedus, pingluns)
 
 
-def writecsv(titles, yuedus, pingluns, wf):
-        ttime=time.strftime("%Y-%m-%d %H:%M", time.localtime())
-        for t, y, p in zip(titles, yuedus, pingluns):
-            wf.write('"'+t+'"'+','+y+','+p+','+ttime+'\n')
+def writecsv(t_ids, titles, yuedus, pingluns, wf):
+        ttime = time.strftime("%Y-%m-%d %H:%M", time.localtime())
+        for t_id, t, y, p in zip(t_ids, titles, yuedus, pingluns):
+            wf.write(t_id + ','+'"'+t+'"'+','+y+','+p+','+ttime+'\n')
 
 
 def main():
@@ -105,20 +117,32 @@ def main():
 
     for gp in gupiaos:
         tiezi_list[gp] = init_tiezi_set(gp)
+        with open('%s.csv' % gp, 'w', encoding='utf-8') as wf:
+            wf.write('id,title,yuedu,pinglun,time\n')
 
     while True:
         print('start')
+        s_time = time.time()
         try:
             for gp in gupiaos:
                 update_tiezi_set(gp)
-                print('Start to get %s: ....'%(gp))
+                print('Start to get %s: ....' % (gp))
                 tiezi_urls = list(tiezi_list[gp])
-                titles, yuedus, pingluns = get_yueping(tiezi_urls)
-                with open('%s.csv'%gp, 'a', encoding='utf-8') as wf:
-                    writecsv(titles, yuedus, pingluns, wf)
+                t_ids, titles, yuedus, pingluns = get_yueping(tiezi_urls)
+                with open('%s.csv' % gp, 'a', encoding='utf-8') as wf:
+                    writecsv(t_ids, titles, yuedus, pingluns, wf)
             print('end')
-            time.sleep(300)
-        except:
+
+            wait_time = 300 - (time.time() - s_time)
+            if wait_time > 0:
+                time.sleep(300)
+            else:
+                print('Longer than 5mins: ', len(tiezi_ids))
+                continue
+        except KeyboardInterrupt:
+            return 0
+        except Exception as e:
+            print(e)
             continue
 
 
